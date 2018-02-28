8f43cabe7cb07ed3dd49b3b8a88e017beb4b76df
Timothy04 <timothy_van_peteghem@hotmail.com>
2018-03-01 00:25:50 +0100

    First commit

---
+++ IDRecognizer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace IDRecognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 376);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(430, 13);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(119, 33);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send Once";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.AcceptsReturn = true;
+            this.txtInfo.Location = new System.Drawing.Point(430, 52);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(256, 337);
+            this.txtInfo.TabIndex = 2;
+            this.txtInfo.Text = "The information will come here. Press send once to send 1 image to the api. Press" +
+    " start video to automatically send an image every 15 seconds. (You may have to w" +
+    "ait a few seconds after pressing send).";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start Video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 422);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IDRecognizer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Newtonsoft.Json;
+
+namespace IDRecognizer
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideoSource;
+        Bitmap image;
+        Timer t;
+
+        string _subscriptionKey = "3326bad869ce401994724aaf67b854ea";
+        string _ocrEndpoint = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            FinalVideoSource = new VideoCaptureDevice(VideoCaptureDevices[0].MonikerString);
+            FinalVideoSource.NewFrame += new NewFrameEventHandler(FinalVideoSource_NewFrame);
+            FinalVideoSource.Start();
+
+            t = new Timer();
+            t.Interval = 15 * 1000;
+            t.Tick += T_Tick;
+        }
+
+        void FinalVideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            ReadWords();
+        }
+
+        public async void ReadWords()
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
+
+            // Request parameters.
+            string requestParameters = "language=unk&detectOrientation=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = _ocrEndpoint + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = (byte[])new ImageConverter().ConvertTo(image, typeof(byte[]));
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Handle json
+                List<string> words = new List<string>();
+                var myObject = JsonConvert.DeserializeObject<OcrResult>(contentString);
+                foreach (var region in myObject.regions)
+                    foreach (var line in region.lines)
+                        foreach (var word in line.words)
+                        {
+                            words.Add(word.text);
+                        }
+
+                string textToRead = string.Join(" ", words.ToArray());
+                if (myObject.language != "unk")
+                {
+                    txtInfo.Text = "(language=" + myObject.language + ")";
+                }
+                else
+                {
+                    txtInfo.Text = "language unknown.";
+                }
+
+                txtInfo.Text += "\n" + textToRead;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadWords();
+            t.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            ReadWords();
+        }
+    }
+
+    [Serializable]
+    public class OcrResult
+    {
+        public string language;
+        public float textAngle;
+        public string orientation;
+        public Region[] regions;
+    }
+
+    [Serializable]
+    public class Region
+    {
+        public string boundingBox;
+        public Line[] lines;
+    }
+
+    [Serializable]
+    public class Line
+    {
+        public string boundingBox;
+        public Word[] words;
+    }
+
+    [Serializable]
+    public class Word
+    {
+        public string boundingBox;
+        public string text;
+    }
+}
